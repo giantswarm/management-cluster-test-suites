a1f4dde848cb798062e490cacbdf61a4be3d3065
Marcus Noble <AverageMarcus@users.noreply.github.com>
2025-05-30 10:08:43 +0100

    Added common test for worker nodes ready (#74)
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- internal/common/basic.go
+++ internal/common/basic.go
@@ -1,13 +1,16 @@
 package common
 
 import (
+	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2" // nolint
 	. "github.com/onsi/gomega"    // nolint
 
 	"github.com/giantswarm/clustertest/pkg/application"
+	"github.com/giantswarm/clustertest/pkg/client"
 	"github.com/giantswarm/clustertest/pkg/failurehandler"
+	"github.com/giantswarm/clustertest/pkg/logger"
 	"github.com/giantswarm/clustertest/pkg/organization"
 	"github.com/giantswarm/clustertest/pkg/wait"
 	cr "sigs.k8s.io/controller-runtime/pkg/client"
@@ -38,6 +41,58 @@ func RunBasic() {
 					5*time.Second,
 				)).
 				WithTimeout(5 * time.Minute).
+				WithPolling(wait.DefaultInterval).
+				Should(Succeed())
+		})
+
+		It("has all the worker nodes running", func() {
+			values := &application.ClusterValues{}
+			err := state.GetFramework().MC().GetHelmValues(fakeWC.Name, fakeWC.GetNamespace(), values)
+			Expect(err).NotTo(HaveOccurred())
+
+			minNodes := 0
+			maxNodes := 0
+			for _, pool := range values.NodePools {
+				if pool.Replicas > 0 {
+					minNodes += pool.Replicas
+					maxNodes += pool.Replicas
+					continue
+				}
+
+				minNodes += pool.MinSize
+				maxNodes += pool.MaxSize
+			}
+			expectedNodes := wait.Range{
+				Min: minNodes,
+				Max: maxNodes,
+			}
+
+			workersFunc := func() (bool, error) {
+				ok, err := wait.AreNumNodesReadyWithinRange(state.GetContext(), state.GetFramework().MC(), expectedNodes, client.DoesNotHaveLabels{"node-role.kubernetes.io/control-plane"})()
+				if err != nil {
+					logger.Log("failed to get nodes: %s", err)
+					return false, err
+				}
+				if !ok {
+					return false, fmt.Errorf("unexpected number of nodes")
+				}
+				return true, nil
+			}
+
+			Eventually(wait.Consistent(
+				func() error {
+					ok, err := workersFunc()
+					if err != nil {
+						logger.Log("failed to get nodes: %s", err)
+						return err
+					}
+					if !ok {
+						return fmt.Errorf("unexpected number of nodes")
+					}
+					return nil
+				},
+				12, 5*time.Second)).
+				WithTimeout(15 * time.Minute).
 				WithPolling(wait.DefaultInterval).
 				Should(Succeed())
 		})
